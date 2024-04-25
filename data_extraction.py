349aaed1563b599efc7d7a4c8739868867c0a499
Milan Sajiv <78609869+MilanSajiv@users.noreply.github.com>
2024-04-25 16:30:15 +0100

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,56 +1,53 @@
-import boto3
+from database_utils import DatabaseConnector
+import sqlalchemy
 import pandas as pd
 import requests
-import yaml
 import tabula
+import boto3 
 
 class DataExtractor:
-    def extract_from_s3(self, s3_address):
-        try:
-            # Splitting S3 address to bucket name and file name
-            bucket_name, file_key = s3_address.split('s3://')[1].split('/')
-            # Downloading file from S3 bucket
-            s3 = boto3.client('s3')
-            response = s3.get_object(Bucket=bucket_name, Key=file_key)
-            # Reading CSV data into DataFrame
-            df = pd.read_csv(response['Body'])
-            return df
-        except Exception as e:
-            print("Error:", e)
-            return None
 
-    def retrieve_pdf_data(self, link):
-        try:
-            dfs = tabula.read_pdf(link, pages='all', multiple_tables=True)
-            df = pd.concat(dfs)
-            return df
-        except Exception as e:
-            print("Error extracting data from PDF:", e)
-            return None
+    def __init__(self):
+        self.db = DatabaseConnector()
+        self.rds_database = self.db.init_db_engine()
+        self.api_key ={'x-api-key':'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
 
-    def list_number_of_stores(self, endpoint, headers):
-        try:
-            response = requests.get(endpoint, headers=headers)
-            if response.status_code == 200:
-                return response.json()['number_of_stores']
-            else:
-                print("Failed to retrieve number of stores from API.")
-                return None
-        except Exception as e:
-            print("Error:", e)
-            return None
-
-    def retrieve_stores_data(self, endpoint, headers):
-        try:
-            response = requests.get(endpoint, headers=headers)
-            if response.status_code == 200:
-                data = response.json()['stores']
-                df = pd.DataFrame(data)
-                return df
-            else:
-                print("Failed to retrieve store data from API.")
-                return None
-        except Exception as e:
-            print("Error:", e)
-            return None
+    def read_rds_table(self, table_name):
+      return pd.read_sql_table(table_name, self.rds_database)
     
+    def retrieve_pdf_data(self, pdf_link):
+        pdf_dataframes = tabula.read_pdf(pdf_link, pages="all", multiple_tables=True)
+        return pd.concat(pdf_dataframes, ignore_index=True)
+    
+    def list_number_of_store(self):
+        stores = requests.get('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores', headers=self.api_key)
+        number_of_stores = stores.json()
+        return number_of_stores["number_stores"]
+    
+    def retrieve_stores_data(self):
+        number_of_stores = self.list_number_of_store()
+        stores_list = []
+        for store_number in range(0, number_of_stores):
+            stores_list.append(requests.get('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/'+str(store_number), headers=self.api_key).json())
+        return pd.json_normalize(stores_list)
+    
+    def extract_from_s3(self, address):
+        s3 = boto3.client('s3')
+        bucket = 'data-handling-public'
+        object = 'products.csv'
+        file = 'products.csv'
+        s3.download_file(bucket, object, file)
+        table = pd.read_csv('./products.csv')
+        return table
+    
+    def extract_from_s3_json(self):
+        s3 = boto3.client('s3')
+        bucket = 'data-handling-public'
+        object = 'date_details.json'
+        file = 'date_details.json'
+        s3.download_file(bucket, object, file)
+        table = pd.read_json('./date_details.json')
+        return table
+    
+if __name__ == "__main__":
+    extractor_instance = DataExtractor()
